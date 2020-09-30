fe6705df1f4431ddab586e76c6abdbd493f9fc46
Okky Mahewara <okkymahes@gmail.com>
2020-09-30 14:57:16 +0800

    aaabbbb

--- application/controllers/Karateka.php
+++ application/controllers/Karateka.php
@@ -10,7 +10,6 @@ class Karateka extends CI_Controller
 		$this->load->model('model_config', 'm_config');
 		$this->load->model('Model_biodata', 'm_biodata');
 		$this->load->model('Model_dojo', 'm_dojo');
-		$this->load->model('Model_karate', 'm_karate');
 		$this->load->model('Model_sabuk', 'm_sabuk');
 	}
 
@@ -41,12 +40,12 @@ class Karateka extends CI_Controller
 		$data['link_pengembang'] = $data_config->config_value;
 		// end konten default pada template wajib isi
 
-		$data['biodata'] = $this->m_karate->getAllKarate();
+		$data['biodata'] = $this->m_biodata->getAllBiodata();
 
 		$this->load->view('templates/header', $data);
 		$this->load->view('templates/topbar', $data);
 		$this->load->view('templates/sidebar', $data);
-		$this->load->view('karateka/index', $data);
+		$this->load->view('biodata/index', $data);
 		$this->load->view('templates/footer', $data);
 	}
 
@@ -77,44 +76,45 @@ class Karateka extends CI_Controller
 		$data['link_pengembang'] = $data_config->config_value;
 		// end konten default pada template wajib isi
 
-		$data['nama'] = $this->m_biodata->getBiodataByStatus();
-		$data['sabuk'] = $this->m_sabuk->getAllSabuk();
+		$data['dojo'] = $this->m_dojo->getAllDojo();
 
 		$this->form_validation->set_rules('nama', 'Nama', 'required');
-		$this->form_validation->set_rules('sabuk', 'Sabuk', 'required');
+		$this->form_validation->set_rules('tempat_lahir', 'Tempat Lahir', 'required');
+		$this->form_validation->set_rules('tglahir', 'Tanggal Lahir', 'required');
+		$this->form_validation->set_rules('jnskelamin', 'Jenis Kelamin', 'required');
+		$this->form_validation->set_rules('dojo', 'Dojo', 'required');
 
 		if ($this->form_validation->run() == false) {
 			$this->load->view('templates/header', $data);
 			$this->load->view('templates/topbar', $data);
 			$this->load->view('templates/sidebar', $data);
-			$this->load->view('karateka/formtambah', $data);
+			$this->load->view('biodata/formtambah', $data);
 			$this->load->view('templates/footer', $data);
 		} else {
-			$id = $this->input->post('nama');
 			$nama = $this->input->post('nama');
-			$sabuk = $this->input->post('sabuk');
+			$tempat_lahir = $this->input->post('tempat_lahir');
+			$tglahir = date('Y-m-d', strtotime($this->input->post('tglahir')));
+			$jnskelamin = $this->input->post('jnskelamin');
+			$dojo = $this->input->post('dojo');
 
 			$data = [
-				'biodata' => $nama,
-				'sabuk' => $sabuk,
-				'is_active' => 1
+				'nama' => $nama,
+				'tempat_lahir' => $tempat_lahir,
+				'tgl_lahir' => $tglahir,
+				'jenis_kelamin' => $jnskelamin,
+				'dojo' => $dojo
 			];
 
-			$dt = [
-				'status_karateka' => 1
-			];
-
-			$this->m_karate->tambahDataKarate($data);
-			$this->m_biodata->ubahDataBioForStatus($dt, $id);
+			$this->m_biodata->tambahDataBiodata($data);
 			$this->session->set_flashdata('message', 'ditambah');
 			redirect('log/karateka');
 		}
 	}
 
-	public function detail($id)
+	public function ubah($id)
 	{
 		$data['judul'] = 'Karateka';
-		$data['subjudul'] = 'Detail Karateka';
+		$data['subjudul'] = 'Form Ubah Karateka';
 
 		// untuk session login wajib isi
 		$user = $this->session->userdata('usrname');
@@ -139,134 +139,39 @@ class Karateka extends CI_Controller
 		// end konten default pada template wajib isi
 
 		$data['biodata'] = $this->m_biodata->getBiodataById($id);
-		$data['karate'] = $this->m_karate->getKarateByIdBio($id);
-		$data['dojo'] = $this->m_biodata->getBioDojoById($id);
+		$data['dojo'] = $this->m_dojo->getAllDojo();
 
-		$this->load->view('templates/header', $data);
-		$this->load->view('templates/topbar', $data);
-		$this->load->view('templates/sidebar', $data);
-		$this->load->view('karateka/tabel_detail', $data);
-		$this->load->view('templates/footer', $data);
-	}
-
-	public function ujian($id)
-	{
-		$data['judul'] = 'Karateka';
-		$data['subjudul'] = 'Detail Ujian';
-
-		// untuk session login wajib isi
-		$user = $this->session->userdata('usrname');
-		$data['userlogin'] = $this->m_user->getUserByUser($user);
-		// end untuk session login wajib isi
-
-		// konten default pada template wajib isi
-		$data_config = $this->m_config->getConfig('brand');
-		$data['brand'] = $data_config->config_value;
-
-		$data_config = $this->m_config->getConfig('main_header');
-		$data['main_header'] = $data_config->config_value;
-
-		$data_config = $this->m_config->getConfig('version');
-		$data['version'] = $data_config->config_value;
-
-		$data_config = $this->m_config->getConfig('nama_pengembang');
-		$data['nama_pengembang'] = $data_config->config_value;
-
-		$data_config = $this->m_config->getConfig('link_pengembang');
-		$data['link_pengembang'] = $data_config->config_value;
-		// end konten default pada template wajib isi
-
-		$data['karate'] = $this->m_karate->getKarateById($id);
-
-		$this->form_validation->set_rules('tglujian', 'Tanggal Ujian', 'required');
+		$this->form_validation->set_rules('nama', 'Nama', 'required');
+		$this->form_validation->set_rules('tempat_lahir', 'Tempat Lahir', 'required');
+		$this->form_validation->set_rules('tglahir', 'Tanggal Lahir', 'required');
+		$this->form_validation->set_rules('jnskelamin', 'Jenis Kelamin', 'required');
+		$this->form_validation->set_rules('dojo', 'Dojo', 'required');
 
 		if ($this->form_validation->run() == false) {
 			$this->load->view('templates/header', $data);
 			$this->load->view('templates/topbar', $data);
 			$this->load->view('templates/sidebar', $data);
-			$this->load->view('karateka/ujian', $data);
+			$this->load->view('biodata/formubah', $data);
 			$this->load->view('templates/footer', $data);
 		} else {
-			$id = $this->input->post('id'); // tidak perlu ini diubah
-			$idbio = $this->input->post('idbio'); // tidak perlu ini diubah
-			$tglujian = date('Y-m-d', strtotime($this->input->post('tglujian')));
+			$id = $this->input->post('id');  // tidak perlu ini diubah
+			$nama = $this->input->post('nama');
+			$tempat_lahir = $this->input->post('tempat_lahir');
+			$tglahir = date('Y-m-d', strtotime($this->input->post('tglahir')));
+			$jnskelamin = $this->input->post('jnskelamin');
+			$dojo = $this->input->post('dojo');
 
 			$data = [
-				'tgl_ujian' => $tglujian
+				'nama' => $nama,
+				'tempat_lahir' => $tempat_lahir,
+				'tgl_lahir' => $tglahir,
+				'jenis_kelamin' => $jnskelamin,
+				'dojo' => $dojo
 			];
 
-			$this->m_karate->ubahDataKarate($data, $id);
-			$this->session->set_flashdata('message', 'ditambah');
-			redirect('log/karateka/detail/' . $idbio);
-		}
-	}
-
-	public function ijasah($id)
-	{
-		$data['judul'] = 'Karateka';
-		$data['subjudul'] = 'Detail Ijasah';
-
-		// untuk session login wajib isi
-		$user = $this->session->userdata('usrname');
-		$data['userlogin'] = $this->m_user->getUserByUser($user);
-		// end untuk session login wajib isi
-
-		// konten default pada template wajib isi
-		$data_config = $this->m_config->getConfig('brand');
-		$data['brand'] = $data_config->config_value;
-
-		$data_config = $this->m_config->getConfig('main_header');
-		$data['main_header'] = $data_config->config_value;
-
-		$data_config = $this->m_config->getConfig('version');
-		$data['version'] = $data_config->config_value;
-
-		$data_config = $this->m_config->getConfig('nama_pengembang');
-		$data['nama_pengembang'] = $data_config->config_value;
-
-		$data_config = $this->m_config->getConfig('link_pengembang');
-		$data['link_pengembang'] = $data_config->config_value;
-		// end konten default pada template wajib isi
-
-		$data['karate'] = $this->m_karate->getKarateById($id);
-		$data['bio'] = $this->m_biodata->getAllBiodata();
-		$data['sabuk'] = $this->m_sabuk->getAllSabuk();
-
-		$this->form_validation->set_rules('tglijasah', 'Tanggal Ijasah', 'required');
-		$this->form_validation->set_rules('noijasah', 'No. Ijasah', 'required');
-		$this->form_validation->set_rules('nilairata', 'Nilai Rata', 'required');
-
-		if ($this->form_validation->run() == false) {
-			$this->load->view('templates/header', $data);
-			$this->load->view('templates/topbar', $data);
-			$this->load->view('templates/sidebar', $data);
-			$this->load->view('karateka/ijasah', $data);
-			$this->load->view('templates/footer', $data);
-		} else {
-			$id = $this->input->post('id'); // tidak perlu ini diubah
-			$idbio = $this->input->post('idbio'); // tidak perlu ini diubah
-			$tglijasah = date('Y-m-d', strtotime($this->input->post('tglijasah')));
-			$noijasah = $this->input->post('noijasah');
-			$nilairata = str_replace(',', '.', $this->input->post('nilairata'));
-			$sabukbaru = $this->input->post('sabukbaru');
-
-			$data = [
-				'tgl_ijasah' => $tglijasah,
-				'no_ijasah' => $noijasah,
-				'nilai_rata' => $nilairata,
-				'is_active' => 0
-			];
-
-			$dt = [
-				'biodata' => $idbio,
-				'sabuk' => $sabukbaru,
-				'is_active' => 1
-			];
-
-			$this->m_karate->ubahDataKarate($data, $id);
-			$this->m_karate->tambahDataKarateNewSabuk($dt);
-			$this->session->set_flashdata('message', 'ditambah');
-			redirect('log/karateka/detail/' . $idbio);
+			$this->m_biodata->ubahDataBiodata($data, $id);
+			$this->session->set_flashdata('message', 'diubah');
+			redirect('log/karateka');
 		}
 	}
 }
